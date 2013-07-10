9fb305e4ae1db2107dac61fb01ced652892b9bed
Ryan Hennig <mindcoder@gmail.com>
2013-07-10 08:46:29 +0000

    added utf-8 specification

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync("index.html");
-  response.send(buffer.toString());
+  var content = buffer.toString("utf-8");
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
